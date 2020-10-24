6f765f21a0917f8a5157daba9eba512e625e794a
MaxLepesevich <73343956+MaxLepesevich@users.noreply.github.com>
2020-10-24 14:14:48 +0300

    N-number of Fibanachi

---
+++ N-number of Fibanachi/N-number of Fibanachi/N-number of Fibanachi.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+unsigned long long int n, F;
+
+int main()
+{
+	cin >> n;
+	F = (pow(((1 + sqrt(5)) / 2), n) - pow(((1 - sqrt(5)) / 2), n)) / sqrt(5);
+	cout << F;
+}
