5740004994f74cefaa32aa3d252d64c5dd24d5ce
MaxLepesevich <73343956+MaxLepesevich@users.noreply.github.com>
2020-10-24 14:18:34 +0300

    Danger Barel 1-2

---
+++ Danger barel-2/Danger barel-2/Danger barel-2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+const int maxn = 30;
+using namespace std;
+int a[maxn];
+
+int main()
+{
+    a[1] = 3;
+    a[2] = 8;
+    int N;
+    cin >> N;
+
+    for (int i = 3; i <= N;i++) {
+        a[i] = (a[i-2] + a[i -1])*2;
+    }
+    cout << a[N];
+}
---
+++ Danger barel/Danger barel/Danger barel.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+const int maxn = 30;
+using namespace std;
+int a[maxn];
+
+int main()
+{
+    a[1] = 2;
+    a[2] = 3;
+    int N;
+    cin >> N;
+    
+    for (int i = 3; i <= N;i++) {
+        a[i] = a[i - 2] + a[i - 1];
+    }
+    cout << a[N];
+}
+
